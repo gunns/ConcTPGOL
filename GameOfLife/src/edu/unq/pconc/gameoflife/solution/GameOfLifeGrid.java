676258e274a959709a0fbf865bf99e5dd03929ab
KoruElMago <alumno@huayra.(none)>
2016-10-31 21:15:04 -0300

    modificaciones en clase

--- GameOfLife/src/edu/unq/pconc/gameoflife/solution/GameOfLifeGrid.java
+++ GameOfLife/src/edu/unq/pconc/gameoflife/solution/GameOfLifeGrid.java
@@ -8,6 +8,7 @@ import edu.unq.pconc.gameoflife.CellGrid;
 
 public class GameOfLifeGrid implements CellGrid {
 	private List<List<Boolean>> tabla = new ArrayList<List<Boolean>>();
+	private List<List<Boolean>> tablaSiguiente = new ArrayList<List<Boolean>>();
 	private int x = 0;
 	private int y = 0;
 	private int gen = 0;
@@ -21,7 +22,7 @@ public class GameOfLifeGrid implements CellGrid {
 	}
 
 	@Override
-	public void setCell(int col, int row, boolean cell) {
+	public synchronized void setCell(int col, int row, boolean cell) {
 		this.tabla.get(col).set(row,cell);
 		
 	}
@@ -48,8 +49,12 @@ public class GameOfLifeGrid implements CellGrid {
 	}
 
 	@Override
-	public void next() {
+	public synchronized void next() {
 		this.gen = this.gen+1;
+		//aca van los threads
+		//se terminan los threads y avisan
+		this.tabla=new ArrayList<List<Boolean>>();
+		this.tabla.addAll(this.tablaSiguiente);
 		// TODO Auto-generated method stub
 		
 	}
@@ -58,19 +63,37 @@ public class GameOfLifeGrid implements CellGrid {
 	public void resize(int col, int row) {
 		List<List<Boolean>> nuevaTabla =new ArrayList<List<Boolean>>();
 		int i = 0;
-		while (i<=col){
-			int r = 0;
-			nuevaTabla.add(new ArrayList<Boolean>());
-			while (r<=row){
-				if (this.tabla.get(i).get(r)!= null){
-					nuevaTabla.get(i).add(r, this.tabla.get(i).get(r));
-				}else{
-					nuevaTabla.get(i).add(r, false);
+		if(this.tabla ==null){
+			while (i<=col){
+				int r=0;
+				nuevaTabla.add(i,new ArrayList<Boolean>());
+				while (r<row){
+					nuevaTabla.get(i).add(r,false);
+					r=r+1;
 				}
+			i=i+1;
 			}
-			
 		}
+		else{
+			while (i<=col-1){
+				int r = 0;
+				nuevaTabla.add(i, new ArrayList<Boolean>());
+				while (r<=row-1){
+					if (this.tabla.get(i)!= null){
+						if(this.tabla.get(i).get(r)!=null){
+							nuevaTabla.get(i).add(r, this.tabla.get(i).get(r));
+						}
+					}else{
+						nuevaTabla.get(i).add(r, false);
+					}
+				}
+				r=r+1;
+			}
+			i=i+1;
+		}	
 		this.tabla = nuevaTabla;
+		this.tablaSiguiente = new ArrayList<List<Boolean>>();
+		this.tablaSiguiente.addAll(nuevaTabla);
 		this.x = col;
 		this.y = row;
 	}
--- GameOfLife/src/edu/unq/pconc/gameoflife/solution/GameOfLifeGridEvaluator.java
+++ GameOfLife/src/edu/unq/pconc/gameoflife/solution/GameOfLifeGridEvaluator.java
@@ -1,5 +1,58 @@
 package edu.unq.pconc.gameoflife.solution;
 
 public class GameOfLifeGridEvaluator {
+	GameOfLifeGrid golG;
+	
+	public void evaluarCelda(int col, int row){
+		Boolean cell= this.golG.getCell(col, row);
+		if (!cell){
+			this.golG.setCell(col, row, (this.cantVecinas(col, row)==3));
+		}
+	}
 
+	private int cantVecinas(int col, int row) {
+		int cant = 0;
+		if(row!=0){
+			cant=cant + this.filaEvaluada(col, row-1);
+		}
+		cant = cant + this.vecinasDeFilas(col, row);
+		if(row!=this.golG.getDimension().height){
+			cant=cant+ this.filaEvaluada(col, row+1);
+		}
+		return cant;
+	}
+
+
+	private int vecinasDeFilas(int col, int row){
+		int cant = 0;
+		if(col != 0){
+			if(this.golG.getCell(col-1, row)){
+				cant=cant+1;
+			}
+		}
+		if(col != this.golG.getDimension().width){
+			if(this.golG.getCell(col+1, row)){
+				cant=cant+1;
+			}
+		}
+		return cant;
+	}
+	
+	private int filaEvaluada(int col, int row){
+		int cant =0;
+		if (col !=0){
+			if (this.golG.getCell(col-1, row)){
+				cant = cant+1;
+			}
+		}
+		if (this.golG.getCell(col, row)){
+			cant=cant+1;
+		}
+		if(col != this.golG.getDimension().width){
+			if(this.golG.getCell(col+1, row)){
+				cant=cant+1;
+			}
+		}
+		return cant;
+	}
 }
